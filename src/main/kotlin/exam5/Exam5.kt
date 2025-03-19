89b7f642d3b39917dddf11c01fb7db9bd6ce41db
kozae00 <rhwogus777@gmail.com>
2025-03-19 14:22:48 +0900

    constructor

---
+++ src/main/kotlin/exam5/Exam5.kt
@@ -0,0 +1,22 @@
+package exam5
+
+class Person(
+    var name: String = "",
+    var age: Int = 0
+) {
+
+    fun greet() {
+        println("Hello, my name is " + name)
+        println("and my age is " + age)
+
+        println("Hello, my name is $name")
+        print("and my age is $age")
+    }
+}
+
+fun main() {
+
+    val person = Person("Alice", 20)
+
+    person.greet()
+}
