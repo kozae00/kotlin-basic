2e4c931f52689fd302f48d87b19c7104145f17d5
kozae00 <rhwogus777@gmail.com>
2025-03-19 14:48:35 +0900

    null 값 해결

---
+++ src/main/kotlin/exam8/Exam8.kt
@@ -0,0 +1,9 @@
+package exam8
+
+fun main() {
+    val name: String ?= "hello"
+    println(name?.length ?: "Name is null")
+
+//    val person = Person()
+//    if (person?.leftArm?.hand?.grab())
+}
