6f7a026430b9bae0cbad9857944d5aa393763dd4
kozae00 <rhwogus777@gmail.com>
2025-03-20 14:19:10 +0900

    interface

---
+++ src/main/kotlin/exam30/exam30.kt
@@ -0,0 +1,24 @@
+package exam30
+
+interface Animal {
+    fun eat()
+}
+
+interface Pet {
+    fun play()
+}
+
+class Dog : Animal, Pet {
+    override fun eat() {
+        TODO("Not yet implemented")
+    }
+
+    override fun play() {
+        TODO("Not yet implemented")
+    }
+
+}
+
+fun main() {
+
+}
