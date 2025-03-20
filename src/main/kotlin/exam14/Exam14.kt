b1bcdfdc0a8ff5631715a754ba8bc955209e9de3
kozae00 <rhwogus777@gmail.com>
2025-03-20 09:26:06 +0900

    중괄호를 활용해 범수의 범위 지정

---
+++ src/main/kotlin/exam14/Exam14.kt
@@ -0,0 +1,9 @@
+package exam14
+
+fun main() {
+    val name = "Alice"
+    val age = 25
+
+    println("안녕하세요 저는 $name 입니다. 나이는 $age 살 입니다.")
+    println("안녕하세요 저는 ${name}입니다. 나이는 ${age}살 입니다.")
+}
