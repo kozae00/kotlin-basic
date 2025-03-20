06b37907b1017b0224d2ba2ccb92e074c71133b0
kozae00 <rhwogus777@gmail.com>
2025-03-20 09:15:52 +0900

    singleton : object

---
+++ src/main/kotlin/exam13/Exam13.kt
@@ -0,0 +1,25 @@
+package exam13
+
+class Test() {
+
+}
+
+object Singleton  {
+    fun showMessage() {
+        println("Hello World")
+    }
+}
+
+fun main() {
+    Singleton.showMessage()
+
+    val a = Singleton
+    val b = Singleton
+
+    println(a == b)
+
+    val t1 = Test()
+    val t2 = Test()
+
+    println(t1 == t2)
+}
