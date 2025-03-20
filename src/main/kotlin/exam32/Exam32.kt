b63f1d1a829dc98fb021e488f51efc351723eb20
kozae00 <rhwogus777@gmail.com>
2025-03-20 14:42:39 +0900

    Setter만 있는 경우 (Getter 호출 시 예외 발생)

--- src/main/kotlin/exam32/Exam32.kt
+++ src/main/kotlin/exam32/Exam32.kt
@@ -1,2 +1,21 @@
 package exam32
 
+class Person {
+    private var _name: String = ""
+
+    var name: String
+        get() = throw IllegalAccessException("Can't access")
+        set(value) {
+            _name = value
+        }
+
+}
+
+fun main() {
+
+    val p = Person()
+
+    p.name = "Bob"
+    println(p.name)
+
+}
